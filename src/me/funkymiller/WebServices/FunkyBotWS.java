c37f1e811b37b28702c578db8725201d1ada412e
AUSTRALIA\DMiller <dmiller@civica.com.au>
2017-06-21 17:11:01 +1000

    Dummy webservice commits

---
+++ src/me/funkymiller/WebServices/FunkyBotWS.java
@@ -0,0 +1,15 @@
+package me.funkymiller.WebServices;
+
+import javax.jws.WebMethod;
+import javax.jws.WebService;
+
+@WebService
+public class FunkyBotWS {
+
+	@WebMethod
+	public String getPoints(String chan) {
+		String jsonPoints = null;
+		
+		return jsonPoints;
+	}
+}
